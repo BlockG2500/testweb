2f0561e935dca8b94833977c285ddfe86dcbc287
BlockG2500 <114137384+BlockG2500@users.noreply.github.com>
2023-06-02 13:00:39 +0000

    h

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
   const code = urlParams.get('code');
-  if (code) { 
-fetchUserProfile();
-alert('found code');
+  if (code) {
+    const SCOPE = 'identify email guilds';
+fetchUserProfile(code, SCOPE)
+    .then(user => {
+      console.log('Discord Name:', user.username);
+      console.log('Discord ID:', user.id);
+      console.log('Discord Email:', user.email);
+    })
+    .catch(error => {
+      console.error('Error fetching user profile:', error);
+    });
+}
   }
 function login() {
   const CLIENT_ID = '1095020466852348024';
@@ -14,11 +23,10 @@ function login() {
   window.location.href = authUrl;
 }
 
-function fetchUserProfile() {
+function fetchUserProfile(code) {
   const CLIENT_ID = '1095020466852348024';
   const CLIENT_SECRET = 'X02Qw8piq25dXxekVNyjzV0paY1_-i-2';
   const REDIRECT_URI = 'https://blockg2500.github.io/testweb/';
-  const SCOPE = 'identify email guilds';
   const tokenUrl = 'https://discord.com/api/oauth2/token';
   const userProfileUrl = 'https://discord.com/api/users/@me';
 
@@ -51,71 +59,13 @@ function fetchUserProfile() {
       const discordName = user.username;
       const discordId = user.id;
       const discordEmail = user.email;
-document.getElementById('discordname').value = discordName;
-document.getElementById('ID').value = discordId;
-document.getElementById('email').value = discordEmail;
       console.log(user);
     })
     .catch(error => {
-      alert("fetch error:" + error)
       console.error('Error fetching user profile:', error);
     });
   })
   .catch(error => {
-    alert("access or code error:" + error)
     console.error('Error exchanging authorization code for access token:', error);
   });
 }
-function fetchUserProfile(storedCode) {
-  const CLIENT_ID = '1095020466852348024';
-  const CLIENT_SECRET = 'X02Qw8piq25dXxekVNyjzV0paY1_-i-2';
-  const REDIRECT_URI = 'https://blockg2500.github.io/testweb/';
-
-  const tokenUrl = 'https://discord.com/api/oauth2/token';
-  const userProfileUrl = 'https://discord.com/api/users/@me';
-
-  const data = new URLSearchParams();
-  data.append('client_id', CLIENT_ID);
-  data.append('client_secret', CLIENT_SECRET);
-  data.append('grant_type', 'authorization_code');
-  data.append('code', storedCode);
-  data.append('redirect_uri', REDIRECT_URI);
-  data.append('scope', SCOPE);
-
-  fetch(tokenUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: data
-  })
-  .then(response => response.json())
-  .then(tokenData => {
-    const accessToken = tokenData.access_token;
-
-    fetch(userProfileUrl, {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    })
-    .then(response => response.json())
-    .then(user => {
-      const discordName = user.username;
-      const discordId = user.id;
-      const discordEmail = user.email;
-document.getElementById('discordname').value = discordname
-document.getElementById('ID').value = discordId
-document.getElementById('email').value = discordEmail
-      console.log(user);
-    })
-    .catch(error => {
-      alert("fetch error:" + error)
-      console.error('Error fetching user profile:', error);
-    });
-  })
-  .catch(error => {
-    alert("access or code error:" + error)
-    console.error('Error exchanging authorization code for access token:', error);
-  });
-}
-// Check if the code exists in local storage
